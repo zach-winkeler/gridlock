c5b90cd5d96aea206a4544319778ce219c68ae7f
Zachary Winkeler <zwinkele@gmail.com>
2026-03-04 12:55:12 -0500

    Initial recurrence code commit

---
+++ gridlock_polynomial_recurrence.py
@@ -0,0 +1,206 @@
+import itertools
+import time
+from copy import copy
+from functools import cache
+from itertools import combinations
+from math import floor
+
+import networkx as nx
+
+
+class Poly:
+    def __init__(self, cs):
+        trailhead = len(cs)
+        for i in reversed(list(range(len(cs)))):
+            if cs[i] == 0:
+                trailhead = i
+            else:
+                break
+        self.cs = cs[:trailhead]
+
+    def __add__(self, other):
+        return Poly([a + b for a, b in itertools.zip_longest(self.cs, other.cs, fillvalue=0)])
+
+    def __sub__(self, other):
+        return Poly([a - b for a, b in itertools.zip_longest(self.cs, other.cs, fillvalue=0)])
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Poly([other * c for c in self.cs])
+        else:
+            d = len(self.cs) - 1 + len(other.cs) - 1
+            zipped = list(itertools.zip_longest(self.cs, other.cs, [0]*(d+1), fillvalue=0))
+            return Poly([sum(zipped[i][0] * zipped[n-i][1] for i in range(n+1)) for n in range(d+1)])
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __repr__(self):
+        return " + ".join([f"{c}k^{i}" for i, c in enumerate(self.cs)])
+
+    def __hash__(self):
+        return hash(self.cs)
+
+    def __eq__(self, other):
+        return self.cs == other.cs
+
+    def evaluate(self, v):
+        return sum(c * (v ** i) for i, c in enumerate(self.cs))
+
+    def degree(self):
+        return max(i for i, c in enumerate(self.cs) if c != 0)
+
+
+zero = Poly([])
+
+
+def k(power):
+    return Poly([0, ] * power + [1])
+
+
+def initialize_from_set(elems):
+    return Partition({e: i for i, e in enumerate(elems)},
+                     {i: e for i, e in enumerate(elems)},
+                     [i for i in range(len(elems))])
+
+
+class Partition:
+    def __init__(self, to_num, from_num, parts):
+        self.to_num = to_num
+        self.from_num = from_num
+        self.parts = parts
+
+    def copy(self):
+        return Partition(copy(self.to_num), copy(self.from_num), copy(self.parts))
+
+    def join_two(self, e1, e2):
+        out = self.copy()
+        i1, i2 = self.to_num[e1], self.to_num[e2]
+        p1, p2 = self.parts[i1], self.parts[i2]
+        if p1 < p2:
+            for i, p in enumerate(self.parts):
+                if p == p2:
+                    out.parts[i] = p1
+        elif p2 < p1:
+            for i, p in enumerate(self.parts):
+                if p == p1:
+                    out.parts[i] = p2
+
+        return out
+
+    def join_many(self, es):
+        out = self.copy()
+        for e in es[1:]:
+            out = out.join_two(es[0], e)
+        return out
+
+    def find(self, e):
+        return self.parts[self.to_num[e]]
+
+    def __len__(self):
+        return len(set(self.parts))
+
+    def __hash__(self):
+        return sum(p * (29 ** i) for i, p in enumerate(self.parts))
+
+    def __eq__(self, other):
+        return all(p == other.parts[i] for i, p in enumerate(self.parts))
+
+
+def lo(g, strategy=lambda g: sorted(g.nodes, key=g.degree, reverse=True)):
+    # constructs initial vertex partition
+    # finds leaves and bivalent vertices to save time
+    def initial_values():
+        partition = initialize_from_set(list(g.nodes))
+
+        # bivalent vertices share the same color as their neighbors
+        bivalent_vertices = [v for v in g.nodes if g.degree(v) == 2]
+        for v in bivalent_vertices:
+            e1, e2 = g.edges(v)
+            partition = partition.join_two(e1[0], e1[1])
+            partition = partition.join_two(e2[0], e2[1])
+
+        # leaves share the same color as their neighbor
+        leaves = [v for v in g.nodes if g.degree(v) == 1]
+        for v in leaves:
+            (e,) = g.edges(v)
+            partition = partition.join_two(e[0], e[1])
+
+        return partition, {v for v in g.nodes if g.degree(v) == 0
+                           or sum(partition.find(v) == partition.find(w) for w in g.neighbors(v))
+                           >= floor(g.degree(v) / 2) + 1}
+
+    # does every vertex share a part with a majority of its neighbors?
+    def locally_optimal(partition, finished):
+        return all(sum(partition.find(v) == partition.find(w) for w in g.neighbors(v))
+                   >= floor(g.degree(v) / 2) + 1 for v in g.nodes if v not in finished)
+
+    # computes the LO polynomial corresponding to the union of the graphs with at least one of the identities added
+    def union_lo(partition, finished, identities):
+        out = zero
+        for i in range(1, len(identities) + 1):
+            for choices in combinations(identities, i):
+                new_partition = partition.copy()
+                for choice in choices:
+                    new_partition = new_partition.join_many(choice)
+                out += (1 if i % 2 == 1 else -1) * lo_helper(new_partition, finished)
+
+        return out
+
+    # updates set of finished vertices before calling the cached helper function
+    def lo_helper(partition, finished):
+        finished = finished.union({v for v in g.nodes
+                                   if v not in finished and
+                                   sum(partition.find(v) == partition.find(w) for w in g.neighbors(v))
+                                   >= floor(g.degree(v) / 2) + 1})
+        return lo_helper_cached(partition, frozenset(finished))
+
+    # this recursive helper function does most of the computation
+    # caches results to save time on repeated calls
+    # requires inputs to be frozen
+    @cache
+    def lo_helper_cached(partition, finished):
+        finished = set(finished)
+        if locally_optimal(partition, finished):
+            return k(len(partition))
+        else:
+            v = next(v for v in strategy(g)
+                     if v not in finished
+                     and sum(partition.find(v) == partition.find(w) for w in g.neighbors(v))
+                     < floor(g.degree(v) / 2) + 1)
+
+            ws = list(g.neighbors(v))
+            same_neighbors = {w for w in ws if partition.find(v) == partition.find(w)}
+            different_neighbors = {w for w in ws if partition.find(v) != partition.find(w)}
+            if len(same_neighbors) == 0:  # at least two neighbors should share a color with v
+                return sum(((1 if j % 2 == 0 else -1) * (j - 1) *
+                           sum((lo_helper(partition.join_many((v,) + new_same_neighbors), finished)
+                               for new_same_neighbors in combinations(different_neighbors, j)), start=zero)
+                           for j in range(2, len(different_neighbors) + 1)), start=zero)
+            elif len(same_neighbors) == 1:  # at least one more neighbor should share a color with v
+                return sum((-(1 if j % 2 == 0 else -1) *
+                           sum((lo_helper(partition.join_many((v,) + new_same_neighbors), finished)
+                               for new_same_neighbors in combinations(different_neighbors, j)), start=zero)
+                           for j in range(1, len(different_neighbors) + 1)), start=zero)
+            else:  # we can't assume any more neighbors share a color with v
+                return lo_helper(partition, finished.union({v})) \
+                    - union_lo(partition,
+                               finished.union({v}),
+                               set(combinations(different_neighbors, len(same_neighbors))).union(
+                                   {(v, w) for w in different_neighbors})) \
+                    + sum((-(1 if j % 2 == 0 else -1) *
+                          sum((lo_helper(partition.join_many((v,) + new_same_neighbors), finished)
+                              for new_same_neighbors in combinations(different_neighbors, j)), start=zero)
+                          for j in range(1, len(different_neighbors) + 1)), start=zero)
+
+    return lo_helper(*initial_values())
+
+
+# g = nx.complete_graph(4)
+# h = nx.complete_graph(4)
+# p = nx.cartesian_product(g, h)
+#
+# start = time.time()
+# print(lo(p))
+# end = time.time()
+# print(end - start)  # ~74s
---
+++ gridlock_polynomial_recurrence_tests.py
@@ -0,0 +1,136 @@
+import unittest
+from random import randrange
+from unittest import TestCase
+
+from networkx import Graph, grid_graph, random_unlabeled_tree, petersen_graph
+from numpy import array
+from scipy.special import stirling2
+
+from gridlock_polynomial_recurrence import lo, Poly, k
+
+
+# computes the falling factorial power of k
+def ff(power):
+    if power == 1:
+        return k(1)
+    else:
+        return Poly([1 - power, 1]) * ff(power - 1)
+
+
+def to_falling_basis(poly):
+    n = poly.degree()
+    mat = array([[stirling2(b, a) for b in range(n + 1)] for a in range(n + 1)])
+    return mat @ poly.coef
+
+
+def from_adjacency_list(adj_list):
+    g = Graph()
+    for v, adj in enumerate(adj_list):
+        g.add_edges_from([(v, w) for w in adj])
+    return g
+
+
+class TestLO(TestCase):
+
+    def test1(self):
+        g = Graph([(1, 2), (1, 3), (2, 3), (4, 5), (4, 6), (5, 6), (7, 8), (7, 9), (8, 9),
+                   (10, 11), (10, 12), (11, 12),
+                   (1, 4), (1, 7), (1, 10), (4, 10), (7, 10)])
+        self.assertEqual(ff(4) + 2 * ff(2) + ff(1), lo(g))
+
+    def test2(self):
+        g = Graph([(1, 2), (1, 3), (2, 3), (4, 5), (4, 6), (5, 6), (7, 8), (7, 9), (8, 9),
+                   (10, 11), (10, 12), (11, 12),
+                   (1, 4), (1, 7), (1, 10), (4, 7), (4, 10), (7, 10)])
+        self.assertEqual(ff(4) + 3 * ff(2) + ff(1), lo(g))
+
+    def test3(self):
+        g = Graph([(1, 2), (1, 3), (1, 4), (4, 5), (4, 6), (4, 7), (7, 8), (7, 9)])
+        self.assertEqual(ff(3) + 2 * ff(2) + ff(1), lo(g))
+
+    def test4(self):
+        g = Graph([(1, 2), (1, 3), (2, 3), (4, 5), (4, 6), (5, 6), (7, 8), (7, 9), (8, 9),
+                   (10, 11), (10, 12), (11, 12), (13, 14), (13, 15), (14, 15), (16, 17), (16, 18), (17, 18),
+                   (1, 5), (4, 8), (7, 11), (10, 14), (13, 17), (16, 2),
+                   (3, 12), (6, 15), (9, 18)])
+        self.assertEqual(k(6), lo(g))
+
+    def test5(self):
+        g = Graph([(1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
+                   (2, 3), (3, 4), (4, 5), (5, 6), (6, 2),
+                   (2, 5), (3, 7), (4, 7), (3, 8), (4, 8)])
+        self.assertEqual(k(2), lo(g))
+
+    def test_grid(self):
+        g = grid_graph(dim=(4, 4))
+        self.assertEqual(k(4) - 2 * k(3) + k(2) + k(1), lo(g))
+
+    def test_path(self):
+        g = Graph([(1, 2), (2, 3), (3, 4), (4, 5),
+                   (1, 6), (1, 7), (1, 8),
+                   (2, 9), (2, 10),
+                   (3, 11), (3, 12),
+                   (4, 13), (4, 14), (4, 15)])
+        self.assertEqual(k(4) - 2 * k(3) + 3 * k(2) - k(1), lo(g))
+
+    def test_tree(self):
+        g = from_adjacency_list([[1], [0, 2, 4], [1, 5], [6], [1, 7], [2], [3, 7], [4, 6, 10], [11], [12], [7, 11, 13],
+                                 [8, 10], [9, 13], [10, 12, 14, 16], [13], [16], [13, 15, 17], [16]])
+
+        self.assertEqual(2 * k(3) - 2 * k(2) + k(1), lo(g))
+
+    def test_petersen(self):
+        g = petersen_graph()
+        self.assertEqual(6 * ff(2) + ff(1), lo(g))
+
+    def test_random_trees(self):
+        for _ in range(10):
+            n = randrange(3, 50)
+            g = random_unlabeled_tree(n)
+            p = lo(g)
+            self.assertEqual(0, p.evaluate(0))
+            self.assertEqual(1, p.evaluate(1))
+            self.assertLess(p.degree(), (n // 3) + 1)
+
+    def test_matt(self):
+        g = from_adjacency_list(
+            [[1, 2, 3, 6], [0, 2, 4, 7], [0, 1, 5, 8], [4, 5, 0, 9], [3, 5, 1, 10], [3, 4, 2, 11], [7, 8, 0, 9],
+             [6, 8, 1, 10], [6, 7, 2, 11], [10, 11, 3, 6], [9, 11, 4, 7], [9, 10, 5, 8]])
+
+        self.assertEqual(k(4) - k(3) - 2 * k(2) + 3 * k(1), lo(g))
+
+    def test_matt2(self):
+        g = from_adjacency_list(
+            [[1, 2, 4, 7], [0, 2, 6, 7], [0, 1, 3, 4], [4, 5, 2, 11], [3, 5, 0, 2], [3, 4, 9, 11], [7, 8, 1, 10],
+             [6, 8, 0, 1], [6, 7, 9, 10], [10, 11, 5, 8], [9, 11, 6, 8], [9, 10, 3, 5]])
+
+        self.assertEqual(k(1), lo(g))
+
+    def test_matt3(self):
+        g = from_adjacency_list(
+            [[1, 2, 3, 4, 10], [0, 2, 3, 5, 11], [0, 1, 3, 6, 8], [0, 1, 2, 7, 9], [5, 6, 7, 0, 14], [4, 6, 7, 1, 15],
+             [4, 5, 7, 2, 12], [4, 5, 6, 3, 13], [9, 10, 11, 2, 12], [8, 10, 11, 3, 13], [8, 9, 11, 0, 14],
+             [8, 9, 10, 1, 15], [13, 14, 15, 6, 8], [12, 14, 15, 7, 9], [12, 13, 15, 4, 10], [12, 13, 14, 5, 11]])
+
+        self.assertEqual(2 * k(4) - 6 * k(3) + 14 * k(2) - 9 * k(1), lo(g))
+
+    def test_matt4(self):
+        g = from_adjacency_list(
+            [[1, 2, 3, 4, 5], [0, 2, 3, 8, 9], [0, 1, 3, 4, 5], [0, 1, 2, 8, 9], [5, 6, 7, 0, 2], [4, 6, 7, 0, 2],
+             [4, 5, 7, 12, 14], [4, 5, 6, 12, 14], [9, 10, 11, 1, 3], [8, 10, 11, 1, 3], [8, 9, 11, 13, 15],
+             [8, 9, 10, 13, 15], [13, 14, 15, 6, 7], [12, 14, 15, 10, 11], [12, 13, 15, 6, 7], [12, 13, 14, 10, 11]])
+
+        self.assertEqual(2 * k(4) + 8 * k(3) - 16 * k(2) + 7 * k(1), lo(g))
+
+    # ladder-shaped graph, non-alternating LO-polynomial
+    def test_nonalternating(self):
+        g = Graph(
+            [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5),
+             (6, 7), (7, 8), (8, 9), (9, 10), (10, 11),
+             (0,6), (1, 7), (2, 8), (3, 9), (4, 10), (5, 11)])
+
+        self.assertEqual(ff(3) + 4 * ff(2) + ff(1), lo(g))
+
+
+if __name__ == '__main__':
+    unittest.main()
